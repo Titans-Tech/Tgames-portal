bcf595f8df7ce43bff86a89dbd19634393f4005c
Yudi <abd.rahmat.wahyudi@gmail.com>
2023-04-04 10:20:51 +0700

    reverse footer

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,11 @@
 import Image from 'next/image'
-import Header from '../components/header'
-import Footer from '../components/footer'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 export default function About() {
   return (
     <>
-      {/* <Header/>
+      <Header/>
       <div className={styles.container}>
         <main className={styles.main}>
           <h1 className={styles.title}>
@@ -14,7 +14,7 @@ export default function About() {
           <p>This is about page! <Image src="/images/vercel.svg" width={100} height={25} alt=""/></p>
         </main>
       </div>
-      <Footer/> */}
+      <Footer/>
     </>
   )
 }
